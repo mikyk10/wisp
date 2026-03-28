daf97a0b4d79e6b74181eae2b230693781c99b40
mikyk10 <4987502+mikyk10@users.noreply.github.com>
2026-03-28 21:54:41 +0900

    refactor: tag model

--- api/app/domain/model/tag.go
+++ api/app/domain/model/tag.go
@@ -7,13 +7,13 @@ type Tag struct {
 	ID             PrimaryKey `gorm:"primaryKey;autoIncrement"`
 	NameNormalized string     `gorm:"type:varchar(128);not null;uniqueIndex"`
 	DisplayName    string     `gorm:"type:varchar(128);not null"`
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
+	CreatedAt      time.Time  `gorm:"not null;"`
+	UpdatedAt      time.Time  `gorm:"not null;"`
 }
 
 // ImageTag links an image to a tag.
 type ImageTag struct {
 	ImageID   PrimaryKey `gorm:"primaryKey"`
 	TagID     PrimaryKey `gorm:"primaryKey"`
-	CreatedAt time.Time
+	CreatedAt time.Time  `gorm:"not null;"`
 }
